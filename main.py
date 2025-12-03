26b908defc1a5c78023245985c95dbe83cd9a81c
WilsonDenis <wilsonvry@gmail.com>
2025-12-04 00:37:38 +0100

    features: get_img_pixel()implementation

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from PIL import Image
 
-img=Image.open("shop.png")
-img.show()
+picture=Image.open("shop.png").convert("RGB")
+
+
+def get_img_pixel(x,y,image):
+    
+    pixels=image.getpixel((x,y))
+    if pixels != "none":
+        print(pixels)
+    else:
+        print("No conversion")
+    return pixels
+get_img_pixel(12,40,picture)
+
